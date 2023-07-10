4b864aa955068544288a0008821199bb41e2331e
iam-player <iampl4y3r@gmail.com>
2023-07-10 12:26:03 +0200

    feature: latest results

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,12 +1,26 @@
 let output = "";
 let result = document.getElementById("result");
+
+let latestResult = document.getElementById("latestResult");
+
 const buttons = document.querySelectorAll("button.button__input");
 const operators = ["+", "-", "*", "/", "%", "="];
 
+const addToLocalStorage = () => {
+    localStorage.setItem("data", result.value);
+    console.log(result.value);
+};
 
 const calculate = inputValue => {
     if(inputValue === "=" && output !== ""){
+        if(output.slice(-1) === "-"){
+            output = eval(output.replace(/[-]{2,}|[+]{2,}|[/]{2,}/g, '+'));
+        }
+
         output = eval(output);
+        addToLocalStorage(output);
+        latestResult.innerHTML += "<li>" + output + "</li>";
+
         try {
             if(!isFinite(output)){
                 output = "Can't div by 0!";
